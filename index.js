13ea67028f3bdb03a7bcb90486229b1dd90e6efa
Shahinur2010 <shahinurmony4@gmail.com>
2023-06-14 23:52:11 +0600

    get read patch for user collection done

--- index.js
+++ index.js
@@ -78,6 +78,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/users/admin/:email', verifyJWT, async(req, res)=>{
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = {admin: user?.role === 'admin'}
+      res.send(result)
+    })
+
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
@@ -89,6 +102,19 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result)
     });
+
+    app.get('/users/instructor/:email', verifyJWT, async(req, res)=>{
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({instructor: false})
+      }
+
+      const query = {email: email}
+      const user = await usersCollection.findOne(query);
+      const result = {instructor: user?.role === 'instructor'}
+      res.send(result)
+    })
 
     app.patch("/users/instructor/:id", async (req, res) => {
       const id = req.params.id;
